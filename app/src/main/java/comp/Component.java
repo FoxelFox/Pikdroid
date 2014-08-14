4e03eab2edcdc9d536241107452728ecccd388a3
FoxelFox <storkner@gmail.com>
2014-08-14 11:30:27 +0200

    Implemented abstract class Component

--- app/src/main/java/comp/Component.java
+++ app/src/main/java/comp/Component.java
@@ -3,5 +3,14 @@ package comp;
 /**
  * Created by Foxel on 13.08.2014.
  */
-public interface Component {
+public abstract class Component {
+    private int id;
+
+    public Component (int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return  id;
+    }
 }
