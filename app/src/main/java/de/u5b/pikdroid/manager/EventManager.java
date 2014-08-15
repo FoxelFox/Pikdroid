df8adb7e2655ea9a8a6bfa940f8159e5c7056cb5
FoxelFox <storkner@gmail.com>
2014-08-15 19:37:21 +0200

    Implemented subscribe method for EventManager #2

--- app/src/main/java/de/u5b/pikdroid/manager/EventManager.java
+++ app/src/main/java/de/u5b/pikdroid/manager/EventManager.java
@@ -1,7 +1,32 @@
 package de.u5b.pikdroid.manager;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import de.u5b.pikdroid.system.*;
+
 /**
  * Created by Foxel on 14.08.2014.
  */
 public class EventManager {
+    private HashMap<EventTopic, ArrayList<ISystem>> subscriber;
+
+    public EventManager() {
+        subscriber = new HashMap<EventTopic, ArrayList<ISystem>>();
+    }
+
+    /**
+     * Subscribe to an EventTopic
+     * @param eTopic Topic to subscribe on
+     * @param system The ISystem that want to subscribe
+     */
+    public void subscribe(EventTopic eTopic, ISystem system) {
+        if(subscriber.containsKey(eTopic)) {
+            subscriber.get(eTopic).add(system);
+        } else {
+            ArrayList<ISystem> list = new ArrayList<ISystem>();
+            list.add(system);
+            subscriber.put(eTopic, list);
+        }
+    }
 }
--- app/src/main/java/de/u5b/pikdroid/system/ISystem.java
+++ app/src/main/java/de/u5b/pikdroid/system/ISystem.java
@@ -3,5 +3,5 @@ package de.u5b.pikdroid.system;
 /**
  * Created by Foxel on 13.08.2014.
  */
-public interface System {
+public interface ISystem {
 }
