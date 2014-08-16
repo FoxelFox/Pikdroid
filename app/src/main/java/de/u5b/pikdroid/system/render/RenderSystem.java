53636056af1f26ccb81d2847c81a5c52d40263f7
FoxelFox <storkner@gmail.com>
2014-08-17 00:55:23 +0200

    Drawing the first triangle

--- app/src/main/java/de/u5b/pikdroid/system/render/RenderSystem.java
+++ app/src/main/java/de/u5b/pikdroid/system/render/RenderSystem.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -15,12 +19,37 @@ public class RenderSystem implements GLSurfaceView.Renderer {
             "uniform mat4 uMVPMatrix;                        \n" +
                     "attribute vec4 vPosition;               \n" +
                     "void main(){                            \n" +
-                    "  gl_Position = uMVPMatrix * vPosition; \n" +
+                    "  gl_Position = vPosition; \n" +
                     "}                                       \n";
+
+    private final String fragmentShaderCode =
+            "precision mediump float;" +
+                    "void main() {" +
+                    "  gl_FragColor = vec4(0.1,0.1,0.1,1.0);" +
+                    "}";
+
+    private int shaderProgram;
+    private FloatBuffer vertexBuffer;
+    static float triangleCoords[] = {   // in counterclockwise order:
+            0.0f,  0.622008459f, 0.0f, // top
+            -0.5f, -0.311004243f, 0.0f, // bottom left
+            0.5f, -0.311004243f, 0.0f  // bottom right
+    };
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         GLES20.glClearColor(1.0f, 0.0f, 0.25f, 1.0f);
+
+        ByteBuffer bb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(triangleCoords);
+        vertexBuffer.position(0);
+
+        shaderProgram = createShader(vertexShaderCode, fragmentShaderCode);
+
+
     }
 
     @Override
@@ -31,5 +60,35 @@ public class RenderSystem implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUseProgram(shaderProgram);
+
+
+
+        int vPosition = GLES20.glGetAttribLocation(shaderProgram,"vPosition");
+        GLES20.glEnableVertexAttribArray(vPosition);
+        GLES20.glVertexAttribPointer(vPosition, 3, GLES20.GL_FLOAT, false, 0, vertexBuffer);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+
+        GLES20.glDisableVertexAttribArray(vPosition);
+
+    }
+
+    private static int createShader(String vertex, String fragment) {
+        int vs = compileShader(GLES20.GL_VERTEX_SHADER, vertex);
+        int fs = compileShader(GLES20.GL_FRAGMENT_SHADER, fragment);
+        int sp = GLES20.glCreateProgram();
+
+        GLES20.glAttachShader(sp, vs);
+        GLES20.glAttachShader(sp, fs);
+        GLES20.glLinkProgram(sp);
+        return sp;
+    }
+
+    private static int compileShader(int type, String code) {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader,code);
+        GLES20.glCompileShader(shader);
+        return shader;
     }
 }
