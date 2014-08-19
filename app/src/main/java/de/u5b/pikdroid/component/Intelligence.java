c0db252206fc271c798d5015c8e30afe154f72c1
FoxelFox <storkner@gmail.com>
2014-08-19 15:22:48 +0200

    Added Intelligence and Movement Component.

---
+++ app/src/main/java/de/u5b/pikdroid/component/Intelligence.java
@@ -0,0 +1,12 @@
+package de.u5b.pikdroid.component;
+
+/**
+ * Created by Foxel on 19.08.2014.
+ */
+public class Intelligence {
+    private int lookupDistance;
+
+    public Intelligence(int lookupDistance) {
+        this.lookupDistance = lookupDistance;
+    }
+}
---
+++ app/src/main/java/de/u5b/pikdroid/component/Movement.java
@@ -0,0 +1,14 @@
+package de.u5b.pikdroid.component;
+
+/**
+ * Created by Foxel on 19.08.2014.
+ */
+public class Movement {
+    private float linearSpeed;
+    private float angularSpeed;
+
+    public Movement(float linearSpeed, float angularSpeed) {
+        this.linearSpeed = linearSpeed;
+        this.angularSpeed = angularSpeed;
+    }
+}
