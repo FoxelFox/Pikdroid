9daeaa738cd3d6eeaf828d54f78a07d518c08266
FoxelFox <storkner@gmail.com>
2014-08-30 13:19:52 +0200

    Created new MainActivity for startup

---
+++ app/src/main/java/de/u5b/pikdroid/android/MainActivity.java
@@ -0,0 +1,30 @@
+package de.u5b.pikdroid.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import de.u5b.pikdroid.R;
+
+/**
+ * Created by Foxel on 30.08.2014.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btn_start_game);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getBaseContext(), PikdroidActivity.class));
+            }
+        });
+    }
+}
